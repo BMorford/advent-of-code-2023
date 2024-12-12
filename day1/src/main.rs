7c3caf2342ad7f669651efbe7bbb701ca2748cd1
BMorford <22627049+BMorford@users.noreply.github.com>
2024-12-12 14:47:49 -0700

    Add day 1 part 1 solution

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -5,7 +5,7 @@ use std::io::BufReader;
 use std::path::Path;
 
 fn main() {
-    let path = Path::new("../../input.txt");
+    let path = Path::new("src/input.txt");
     let file = match File::open(&path) {
         Err(why) => panic!(
             "couldn't open {}: {}",
@@ -27,21 +27,12 @@ fn main() {
             .filter(|c| c.is_digit(RADIX))
             .collect();
 
-        let current_value: Vec<char> = vec![
-            chars[0],
-            match chars.last().copied() {
-                Some(c) => c,
-                None => panic!(),
-            },
-        ];
-        let current_value_number = match current_value
+        let current_value: Vec<char> = vec![chars[0], chars.last().copied().expect("")];
+        let current_value_number = current_value
             .into_iter()
             .map(|c| c.to_digit(RADIX))
             .try_fold(0, |ans, i| i.map(|i| ans * RADIX + i))
-        {
-            None => panic!(),
-            Some(n) => n,
-        };
+            .expect("");
         result.push(current_value_number);
     }
     println!("Sum: {}", result.iter().sum::<u32>());
